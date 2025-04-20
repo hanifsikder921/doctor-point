c9aaa7f9bac859c66d7bfe5e5d439d126168cdc6
Hanif Sikder <mdhanifsikderpersonal@gmail.com>
2025-04-21 00:39:58 +0600

    nav item activator setup Done

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -4,10 +4,10 @@ import { NavLink } from 'react-router';
 const Menu = () => {
     return (
         <ul className="menu menu-horizontal px-1 gap-12">
-            <li><NavLink className={({ isActive }) => isActive ? 'text-indigo-600 border-b-2 border-indigo-600' : ''} to='/'>Home</NavLink></li>
-            <li><NavLink className={({ isActive }) => isActive ? 'text-indigo-600 border-b-2 border-indigo-600' : ''} to='/booking'>My-Booking</NavLink></li>
-            <li><NavLink className={({ isActive }) => isActive ? 'text-indigo-600 border-b-2 border-indigo-600' : ''} to='/blog'>Blog</NavLink></li>
-            <li><NavLink className={({ isActive }) => isActive ? 'text-indigo-600 border-b-2 border-indigo-600' : ''} to='/contact'>Contact Us</NavLink></li>
+            <li><NavLink className={({ isActive }) => isActive ? 'text-indigo-600 border-b-2 border-indigo-600 rounded-none' : ''} to='/'>Home</NavLink></li>
+            <li><NavLink className={({ isActive }) => isActive ? 'text-indigo-600 border-b-2 border-indigo-600 rounded-none' : ''} to='/booking'>My-Booking</NavLink></li>
+            <li><NavLink className={({ isActive }) => isActive ? 'text-indigo-600 border-b-2 border-indigo-600 rounded-none' : ''} to='/blog'>Blog</NavLink></li>
+            <li><NavLink className={({ isActive }) => isActive ? 'text-indigo-600 border-b-2 border-indigo-600 rounded-none' : ''} to='/contact'>Contact Us</NavLink></li>
 
 
         </ul>
